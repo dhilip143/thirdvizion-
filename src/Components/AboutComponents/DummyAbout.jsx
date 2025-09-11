4922aa92ed791ee6ed4fca253e081251bb18163d
dhilip143 <dhilip146@gmail.com>
2025-09-11 18:43:02 +0530

    installed lenis

--- src/Components/AboutComponents/DummyAbout.jsx
+++ src/Components/AboutComponents/DummyAbout.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function About() {
   const containerRef = useRef(null);
