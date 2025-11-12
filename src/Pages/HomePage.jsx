351d52d71af3a8bab040995c0fb9f9dcca2b1adf
dhilip143 <kdhilip146@gmail.com>
2025-11-12 15:08:18 +0530

    pul

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Hero from "/src/Components/HomeComponent/HeroSection/HeroSection.jsx";
 import VisionMission from "/src/Components/HomeComponent/VissionSection.jsx";
 import Industries from "/src/Components/HomeComponent/Industries.jsx";
 import Newservice from "/src/Components/HomeComponent/Newservice.jsx";
-import Indhu from "./Components/HomeComponent/Indhu.jsx";
+// import Indhu from "./Components/HomeComponent/Indhu.jsx";
 import Partners from "/src/Components/AboutComponents/Partners.jsx";
 import Testimonial from "/src/Components/HomeComponent/Testimonial.jsx";
 import FAQ from "/src/Components/HomeComponent/FAQ.jsx";
@@ -56,7 +56,7 @@ export default function Home() {
       <VisionMission />
       <Industries />
       <Newservice />
-      <Indhu />
+      {/* <Indhu /> */}
       <Partners className="bg-black" />
       <Testimonial />
       <FAQ />
