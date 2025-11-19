e39473a26fcce28856c962223b4eaf14232822aa
dhilip143 <kdhilip146@gmail.com>
2025-11-19 15:01:35 +0530

    firt

--- src/Components/HomeComponent/indhu.jsx
+++ src/Components/HomeComponent/indhu.jsx
@@ -7,7 +7,7 @@ import threed from "/src/assets/indh/search.png";
 import gam from "/src/assets/indh/second.png";
 import are from "/src/assets/indh/tool.png";
 import wih from "/src/assets/indh/dounload.png";
-
+import bit from "/src/assets/indh/succes.png";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Indhu() {
@@ -86,7 +86,7 @@ export default function Indhu() {
       label: "SUCCEES",
       description:
         "Success isn't a destination—it's a journey of constant growth. We empower your business to achieve measurable results through strategic execution, data-driven insights, and relentless improvement—ensuring your goals aren't just met, but exceeded.",
-      img: threed, // You can replace this with a new image
+      img:bit , // You can replace this with a new image
       cx: circleSpacing * 5.35 - leftShift, // New fifth circle
       cy: 200,
       color: "#FFC016", // yellow (same as before)
