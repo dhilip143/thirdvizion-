0df055412e3fe147b42c60c70ea29c529fa28664
dhilip143 <kdhilip146@gmail.com>
2025-11-19 14:51:38 +0530

    first

--- src/Components/HomeComponent/indhu.jsx
+++ src/Components/HomeComponent/indhu.jsx
@@ -3,10 +3,10 @@ import React, { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import threed from "/src/assets/HomeImages/g11.svg";
-import gam from "/src/assets/HomeImages/g22.svg";
-import are from "/src/assets/HomeImages/g33.svg";
-import wih from "/src/assets/HomeImages/g44.svg";
+import threed from "/src/assets/indh/search.png";
+import gam from "/src/assets/indh/second.png";
+import are from "/src/assets/indh/tool.png";
+import wih from "/src/assets/indh/dounload.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
